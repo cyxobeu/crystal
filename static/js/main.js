92c7a8e58b1cb7267b0a28e828be93d54d2d7a36
Ilya <cyxobeu@mail.ru>
2017-08-10 12:26:42 +0300

    add other section

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,33 @@
 $(document).ready(function () {
+
+  // ВАЛИДАЦИЯ ТЕЛЕФОНА
+  $("input[type=tel]").inputmask("+7(999)9999999");
+  $(document).on("keydown change", ".phone-mask-input, input[type=tel]", function (e) {
+    var $this = $(this);
+    var val = $this.val();
+    if ((val.indexOf("+7(89") === 0) || (val.indexOf("+7(88") === 0)) {
+      var new_val = val.slice(0, 3) + val.slice(4, val.length);
+      $this.val(new_val);
+      $this.trigger("change", e);
+    }
+  });
+
+
+
+
   $('.menu_button').click(function (e) {
     e.preventDefault();
+    $(this).toggleClass('active');
     $('.menu').toggleClass('active');
+  });
+
+  // ПЛАВНЫЙ СКРОЛЛ ДО ЯКОРЯ
+  $(document).on('click', '.menu_item', function (e) {
+    e.preventDefault();
+
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    $('html, body').animate({scrollTop:destination  - 80},400);
   });
 
 
@@ -22,6 +48,24 @@ $(document).ready(function () {
     asNavFor: '.main_slider_navigation'
   });
 
+
+  $('.plans_content_block .navigation_img_block').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    focusOnSelect: true,
+    asNavFor: '.plans_content_block .main_img_block'
+  });
+  $('.plans_content_block .main_img_block').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: false
+  });
+
+
   if ($('.about_text').innerHeight() > 190) {
     $('.about_text').addClass('big')
   }
@@ -32,8 +76,6 @@ $(document).ready(function () {
       $('.about_text').removeClass('big')
     }
   });
-
-
   $('.about_text .view_more').click(function (e) {
     e.preventDefault();
 
@@ -86,5 +128,40 @@ $(document).ready(function () {
       }
     ]
   });
+
+  //        СКРОЛЛ ДО ВЕРХА
+  $(document).on('click', '.footer_anchor', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0},400);
+  });
+
+  // ПЕРЕКЛЮЧЕНИЕ КНОПОК ПЛАНИРОВКИ
+  $(document).on('click', '.plans_button', function (e) {
+    e.preventDefault();
+    var $data_info = $(this).attr('data-room-number');
+    var $cell = $('.plans_content_block .plans_content_cell');
+
+    if ($(this).hasClass('all')) {
+      $(this).siblings('.plans_button').removeClass('active');
+      $(this).addClass('active');
+
+      $cell.removeClass('hidden');
+    } else if ($(this).hasClass('floor')) {
+      $(this).siblings('.plans_button').removeClass('active');
+      $(this).addClass('active');
+
+      $cell.addClass('hidden');
+      $('.plans_content_block .plans_content_cell[data-room-number="floor"]').removeClass('hidden');
+    } else {
+      $(this).siblings('.plans_button').not('.room').removeClass('active');
+      $(this).toggleClass('active');
+      if (!$(this).hasClass('active')) {
+        $('.plans_content_block .plans_content_cell[data-room-number=' + $data_info + ']').addClass('hidden');
+      } else {
+        $cell.addClass('hidden');
+        $('.plans_content_block .plans_content_cell[data-room-number=' + $data_info + ']').removeClass('hidden');
+      }
+    }
+  })
 });
 
